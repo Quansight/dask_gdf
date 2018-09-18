533f59b70d578cadd2200ea2fea73e4d8a3bd002
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-09-18 06:04:10 -0400

    Add categorical accessor

--- dask_gdf/tests/test_accessor.py
+++ dask_gdf/tests/test_accessor.py
@@ -5,46 +5,74 @@ from pygdf.dataframe import Series
 import dask_gdf as dgd
 import pandas as pd
 
+#############################################################################
+#                        Datetime Accessor                                  #
+#############################################################################
 
-def data():
+
+def data_dt_1():
     return pd.date_range('20010101', '20020215', freq='400h')
 
 
-def data1():
+def data_dt_2():
     return np.random.randn(100)
 
 
-fields = ['year', 'month', 'day', 'hour', 'minute', 'second']
+dt_fields = ['year', 'month', 'day', 'hour', 'minute', 'second']
 
 
-@pytest.mark.parametrize('data', [data1()])
+@pytest.mark.parametrize('data', [data_dt_2()])
 @pytest.mark.xfail(raises=AttributeError)
 def test_datetime_accessor_initialization(data):
-    pd_data = pd.Series(data.copy())
-    gdf_data = Series(pd_data)
-    dask_gdf_data = dgd.from_pygdf(gdf_data, npartitions=5)
-    dask_gdf_data.dt
+    pdsr = pd.Series(data.copy())
+    sr = Series(pdsr)
+    dsr = dgd.from_pygdf(sr, npartitions=5)
+    dsr.dt
 
 
-@pytest.mark.parametrize('data', [data()])
+@pytest.mark.parametrize('data', [data_dt_1()])
 def test_series(data):
-    pd_data = pd.Series(data.copy())
-    gdf_data = Series(pd_data)
-    dask_gdf_data = dgd.from_pygdf(gdf_data, npartitions=5)
+    pdsr = pd.Series(data.copy())
+    sr = Series(pdsr)
+    dsr = dgd.from_pygdf(sr, npartitions=5)
 
     np.testing.assert_equal(
-        np.array(pd_data),
-        np.array(dask_gdf_data.compute()),
+        np.array(pdsr),
+        np.array(dsr.compute()),
     )
 
 
-@pytest.mark.parametrize('data', [data()])
-@pytest.mark.parametrize('field', fields)
+@pytest.mark.parametrize('data', [data_dt_1()])
+@pytest.mark.parametrize('field', dt_fields)
 def test_dt_series(data, field):
-    pd_data = pd.Series(data.copy())
-    gdf_data = Series(pd_data)
-    dask_gdf_data = dgd.from_pygdf(gdf_data, npartitions=5)
-    base = getattr(pd_data.dt, field)
-    test = getattr(dask_gdf_data.dt, field).compute()\
-                                           .to_pandas().astype('int64')
+    pdsr = pd.Series(data.copy())
+    sr = Series(pdsr)
+    dsr = dgd.from_pygdf(sr, npartitions=5)
+    base = getattr(pdsr.dt, field)
+    test = getattr(dsr.dt, field).compute()\
+                                 .to_pandas().astype('int64')
     assert_series_equal(base, test)
+
+
+#############################################################################
+#                        Categorical Accessor                               #
+#############################################################################
+
+
+def data_cat_1():
+    cat = pd.Categorical(['a', 'a', 'b', 'c', 'a'], categories=['a', 'b', 'c'])
+    return cat
+
+
+def data_cat_2():
+    cat = pd.Categorical(['a', '_', '_', 'c', 'a'], categories=['a', 'b', 'c'])
+    return cat
+
+
+@pytest.mark.parametrize('data', [data_cat_2()])
+@pytest.mark.xfail(raises=AttributeError)
+def test_categorical_accessor_initialization(data):
+    pdsr = pd.Series(data.copy())
+    sr = Series(pdsr)
+    dsr = dgd.from_pygdf(sr, npartitions=5)
+    dsr.cat
